f69ad4dbfcc693e078ce4da0c12609eaa55568de
MAvi-Rup <avirupmondal19@gmail.comm>
2024-02-16 20:13:50 +0600

    UI and Title Updated Slightly

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
   });
 
   return (
-    <div className="grid place-items-center bg-slate-300 h-screen px-6 font-sans">
+    <div className="grid place-items-center bg-slate-300 font-sans">
       <Navbar />
       <Counter todos={todos} />
       <div className=" w-full max-w-2xl rounded-lg bg-slate-200 px-5">
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="fixed bg-slate-500 top-0 left-0 text-center w-full text-white font-semibold shadow-md py-5">
+    <div className=" bg-slate-500  text-center w-full text-white font-semibold shadow-md py-5">
       React Todo Application
     </div>
   );
